b3526b340691b2c341a125a9b6d5798d43e868bc
Shivam Kumar <113598431+shivam12006716@users.noreply.github.com>
2023-09-16 13:44:25 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,189 +1,167 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <ctime>
+
 using namespace std;
-#define COMPUTER 1
-#define HUMAN 2
-#define SIDE 3 // Length of the board
-// Computer will move with 'O'
-// and human with 'X'
-#define COMPUTERMOVE 'O'
-#define HUMANMOVE 'X'
-// A function to show the current board status
-void showBoard(char board[][SIDE])
-{
- printf("\n\n");
- 
- printf("\t\t\t %c | %c | %c \n", board[0][0],
- board[0][1], board[0][2]);
- printf("\t\t\t--------------\n");
- printf("\t\t\t %c | %c | %c \n", board[1][0],
- board[1][1], board[1][2]);
- printf("\t\t\t--------------\n");
- printf("\t\t\t %c | %c | %c \n\n", board[2][0],
- board[2][1], board[2][2]);
- return;
+
+#define compareBoxes(box1, box2, box3) ((board[box1] == board[box2]) && (board[box2] == board[box3]) && (board[box1] != 0)) 
+#define numberToLetter(x) ((x > 0) ? (x == 1) ? 'X' : 'O' : ' ')
+
+int getWinner(int board[9]) {
+	int winner = 0;
+	for (int x = 0; x < 3; x++) {
+		if (compareBoxes(3*x, 3*x+1, 3*x+2)) { 
+			winner = board[3*x];
+			break;
+		} else if (compareBoxes(x, x+3, x+6)) { 
+			winner = board[x];
+			break;
+
+		} else if (compareBoxes(2*x, 4, 8-2*x) && (x < 2)) { 
+			winner = board[4];
+			break;
+		}
+	}
+	return winner;
 }
-// A function to show the instructions
-void showInstructions()
-{
- printf("\t\t\t Tic-Tac-Toe\n\n");
- printf("Choose a cell numbered from 1 to 9 as below"
- " and play\n\n");
- 
- printf("\t\t\t 1 | 2 | 3 \n");
- printf("\t\t\t--------------\n");
- printf("\t\t\t 4 | 5 | 6 \n");
- printf("\t\t\t--------------\n");
- printf("\t\t\t 7 | 8 | 9 \n\n");
- 
- printf("-\t-\t-\t-\t-\t-\t-\t-\t-\t-\n\n");
- return;
+bool gameOver(int board[9]){
+
+	int winner = getWinner(board);
+	if (winner > 0) {
+		cout << numberToLetter(winner) << " wins!"<< endl;
+		return true;
+	} 
+	for (int x = 0; x < 9; x++) {
+		if (board[x] == 0) return false;
+	}
+	cout << "Tie!\n\n";
+	return true;
 }
-// A function to initialise the game
-void initialise(char board[][SIDE], int moves[])
-{
- // Initiate the random number generator so that
- // the same configuration doesn't arises
- srand(time(NULL));
- 
- // Initially the board is empty
- for (int i=0; i<SIDE; i++)
- {
- for (int j=0; j<SIDE; j++)
- board[i][j] = ' ';
- }
- 
- // Fill the moves with numbers
- for (int i=0; i<SIDE*SIDE; i++)
- moves[i] = i;
- // randomise the moves
- random_shuffle(moves, moves + SIDE*SIDE);
- 
- return;
+
+int willWin(int board[9], int player) {
+
+	for (int x = 0; x < 9; x++) {
+		int tempBoard[9];
+		memcpy(tempBoard, board, 36);
+		if (board[x] > 0) continue;
+		tempBoard[x] = player;
+		if(getWinner(tempBoard) == player) return x;
+	}
+	return -1;
 }
-// A function to declare the winner of the game
-void declareWinner(int whoseTurn)
-{
- if (whoseTurn == COMPUTER)
- printf("COMPUTER has won\n");
- else
- printf("HUMAN has won\n");
- return;
+
+int exceptionalCase(int board[9]) {
+	
+	int cases[2][9] = {{1,0,0,0,2,0,0,0,1}, {0,1,0,1,2,0,0,0,0}}; 
+	int answers[2][4] = {{3,3,3,3}, {2,8,6,0}};
+	int rotatedBoard[9] = {6,3,0,7,4,1,8,5,2};
+	int newBoard[9];
+	int tempBoard[9];
+	for(int x = 0; x < 9; x++) {
+		newBoard[x] = board[x];
+	}
+	for (int caseIndex = 0; caseIndex < 2; caseIndex++) {
+		for(int rotation = 0; rotation < 4; rotation++) {
+			for (int x = 0; x < 9; x++) 
+				tempBoard[x] = newBoard[x];
+			
+			int match = 0;
+
+			for (int box = 0; box < 9; box++) {
+				newBoard[box] = tempBoard[rotatedBoard[box]];
+			}
+
+			for (int x = 0; x < 9; x++) {
+				if (newBoard[x] == cases[caseIndex][x]) match++;
+				else break;
+			}
+			if (match == 9) return answers[caseIndex][rotation];
+		}
+	}
+	return -1;
 }
-// A function that returns true if any of the row
-// is crossed with the same player's move
-bool rowCrossed(char board[][SIDE])
-{
- for (int i=0; i<SIDE; i++)
- {
- if (board[i][0] == board[i][1] &&
- board[i][1] == board[i][2] &&
- board[i][0] != ' ')
- return (true);
- }
- return(false);
+
+int getSpace(int board[9], int spaces[4]) {
+
+	bool isSpaceEmpty = false;
+	int y;
+	for (int x = 0; x < 4; x++) {
+		if (board[spaces[x]] == 0) {
+			isSpaceEmpty = true;
+			break;
+		}
+	}
+	if (isSpaceEmpty) {
+		do {
+			y = rand() % 4;
+		} while (board[spaces[y]] != 0);
+		return spaces[y];
+	}
+	return -1;
 }
-// A function that returns true if any of the column
-// is crossed with the same player's move
-bool columnCrossed(char board[][SIDE])
-{
- for (int i=0; i<SIDE; i++)
- {
- if (board[0][i] == board[1][i] &&
- board[1][i] == board[2][i] &&
- board[0][i] != ' ')
- return (true);
- }
- return(false);
+
+void outputBoard(int board[9]) {
+	for(int line = 0; line < 3; line++){
+		for (int box = 0; box < 3; box++) {
+			cout << numberToLetter(board[3*line+box]) << ((box < 2) ? '|' : '\n');
+		}
+		cout << ((line < 2) ? "-----\n" : "\n");
+	}
 }
-// A function that returns true if any of the diagonal
-// is crossed with the same player's move
-bool diagonalCrossed(char board[][SIDE])
-{
- if (board[0][0] == board[1][1] &&
- board[1][1] == board[2][2] &&
- board[0][0] != ' ')
- return(true);
- 
- if (board[0][2] == board[1][1] &&
- board[1][1] == board[2][0] &&
- board[0][2] != ' ')
- return(true);
- return(false);
-}
-// A function that returns true if the game is over
-// else it returns a false
-bool gameOver(char board[][SIDE])
-{
- return(rowCrossed(board) || columnCrossed(board)
- || diagonalCrossed(board) );
-}
-// A function to play Tic-Tac-Toe
-void playTicTacToe(int whoseTurn)
-{
- // A 3*3 Tic-Tac-Toe board for playing
- char board[SIDE][SIDE];
- 
- int moves[SIDE*SIDE];
- 
- // Initialise the game
- initialise(board, moves);
- 
- // Show the instructions before playing
- showInstructions();
- 
- int moveIndex = 0, x, y;
- 
- // Keep playing till the game is over or it is a draw
- while (gameOver(board) == false &&
- moveIndex != SIDE*SIDE)
- {
- if (whoseTurn == COMPUTER)
- {
- x = moves[moveIndex] / SIDE;
- y = moves[moveIndex] % SIDE;
- board[x][y] = COMPUTERMOVE;
- printf("COMPUTER has put a %c in cell %d\n",
- COMPUTERMOVE, moves[moveIndex]+1);
- showBoard(board);
- moveIndex ++;
- whoseTurn = HUMAN;
- }
- 
- else if (whoseTurn == HUMAN)
- {
- x = moves[moveIndex] / SIDE;
- y = moves[moveIndex] % SIDE;
- board[x][y] = HUMANMOVE;
- printf ("HUMAN has put a %c in cell %d\n",
- HUMANMOVE, moves[moveIndex]+1);
- showBoard(board);
- moveIndex ++;
- whoseTurn = COMPUTER;
- }
- }
- // If the game has drawn
- if (gameOver(board) == false &&
- moveIndex == SIDE * SIDE)
- printf("It's a draw\n");
- else
- {
- // Toggling the user to declare the actual
- // winner
- if (whoseTurn == COMPUTER)
- whoseTurn = HUMAN;
- else if (whoseTurn == HUMAN)
- whoseTurn = COMPUTER;
- 
- // Declare the winner
- declareWinner(whoseTurn);
- }
- return;
-}
-// Driver program
-int main()
-{
- // Let us play the game with COMPUTER starting first
- playTicTacToe(COMPUTER);
- 
- return (0);
+
+int main(){
+	int board[9] = {0,0,0,0,0,0,0,0,0}; 
+	int possibleWinner;
+	int move;
+	bool isInvalid;
+	string moveString;
+	srand((int) time(0));
+	int corners[4] = {0,2,6,8};
+	int sides[4] = {1,3,5,7};
+
+	cout << "1|2|3\n-----\n4|5|6\n-----\n7|8|9\n\n";
+
+	while (true) {
+
+		do {
+			cout << "X: ";
+			getline(cin, moveString);
+			move = moveString[0] - '1';
+			if (move > 8 || move < 0 || board[move] != 0) {
+				cout << "Invalid input" << endl;
+				isInvalid = true;
+			} else {
+				board[move] = 1;
+				isInvalid = false;
+				cout << endl;
+			}
+		} while (isInvalid);
+
+		if (gameOver(board) > 0) {
+			outputBoard(board);
+			break;
+		}
+
+		bool good = false;
+		for (int x = 2; x > 0; x--){
+			possibleWinner = willWin(board, x);
+			if (possibleWinner != -1) {
+				board[possibleWinner] = 2;
+				good = true;
+				break;
+			}
+		}
+		if (good);
+		else if (board[4] == 0) board[4] = 2;
+		else if (exceptionalCase(board) > -1) board[exceptionalCase(board)] = 2;
+		else if (getSpace(board, corners) != -1) board[getSpace(board, corners)] = 2; 
+		else board[getSpace(board, sides)] = 2; 
+		
+		outputBoard(board);
+
+		if(gameOver(board)) break;
+
+	}
+	return 0;
 }
